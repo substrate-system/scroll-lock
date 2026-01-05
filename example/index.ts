eb6c42cabf2ea665b8866547d831ade4145e7d94
nichoth <nichoth@nichoth.com>
2026-01-05 15:01:59 -0800

    update dependencies

--- example/index.ts
+++ example/index.ts
@@ -2,7 +2,7 @@ import { lockBodyScrolling, unlockBodyScrolling } from '../src/index.js'
 import './index.css'
 import '../src/index.css'
 import Debug from '@substrate-system/debug'
-const debug = Debug()
+const debug = Debug(import.meta.env.DEV)
 
 document.getElementById('lock')?.addEventListener('click', ev => {
     ev.preventDefault()
